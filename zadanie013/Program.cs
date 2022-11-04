28dc5d8a95db2e3cb231b61f70e245e9f1f242be
Aleksandr Onufriev <pskmp@yandex.ru>
2022-11-04 15:58:23 +0300

    dobavil zadanie013

---
+++ zadanie013/Program.cs
@@ -0,0 +1,31 @@
+﻿// I. Реализовать следующие функции для работы с массивами:
+// 13. Подсчёт количества нечётных элементов в массиве
+Console.Clear();
+void FillArray(int[] arr, int min, int max)    
+    {
+        for (int i=0; i<arr.Length; i++)
+            arr[i] = new Random().Next(min, max);
+    }
+void PrintArray(int[] arr)                     
+{
+    for (int i=0; i<arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+int UnevenCount(int[] arr)                   
+{
+    int count=0;
+    for (int i=0; i<arr.Length; i++)
+    {
+        if (arr[i]%2!=0)
+        count++;
+    }
+    return count;
+}
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+int[] arr = new int [size];
+FillArray(arr, 1, 20);
+PrintArray(arr);
+int x = UnevenCount(arr);
+Console.WriteLine($"Количество нечетных элементов в массиве равно: {x} ");
