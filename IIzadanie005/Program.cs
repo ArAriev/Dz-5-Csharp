0143a9c44cb6a628400163cad58c5cdca22d71ae
Aleksandr Onufriev <pskmp@yandex.ru>
2022-11-04 16:42:38 +0300

    dobavil IIzadanie005

---
+++ IIzadanie005/Program.cs
@@ -0,0 +1,15 @@
+﻿// II. Реализовать следующие функции:
+// 5. Функцию, складывающую два целых числа
+Console.Clear();
+int Sum(int a, int b)
+{
+    int sum = 0;            
+    sum = a + b;
+return sum;
+}
+Console.WriteLine("Введите первое целое число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второе целое число: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+int x = Sum(a, b);
+Console.WriteLine($"Сумма чисел {a} и {b} равна: {x}" );
