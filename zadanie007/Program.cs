1e75a9148911c0cdd4a8b8f3ce41f456309f48c9
Aleksandr Onufriev <pskmp@yandex.ru>
2022-11-04 15:34:00 +0300

    dobavil zadanie007

---
+++ zadanie007/Program.cs
@@ -0,0 +1,19 @@
+﻿// I. Реализовать следующие функции для работы с массивами:
+// 7. Печать массива на экран
+Console.Clear();
+void FillArray(int [] arr, int min, int max)    
+    {
+        for (int i=0; i<arr.Length; i++)
+            arr[i] = new Random().Next(min, max);
+    }
+void PrintArray(int[] arr)                     
+{
+    for (int i=0; i<arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+int [] arr = new int [size];
+FillArray(arr, 1, 99);
+PrintArray(arr);
