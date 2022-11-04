9232617c85b25126c1290496b22805ec49a56577
Aleksandr Onufriev <pskmp@yandex.ru>
2022-11-04 15:18:31 +0300

    dobavil zadanie004

---
+++ zadanie004/Program.cs
@@ -0,0 +1,31 @@
+﻿// I. Реализовать следующие функции для работы с массивами:
+//4. Поиск произведения элементов массива
+Console.Clear();
+void FillArray(int[] arr, int min, int max)    
+    {
+        for (int i=0; i<arr.Length; i++)
+            arr[i] = new Random().Next(min, max);
+    }
+
+void PrintArray(int[] arr)                    
+{
+    for (int i=0; i<arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int Multy(int[] arr)                            
+{
+    int s = 1;
+    for (int i=0; i<arr.Length; i++)
+        s = s * arr[i];
+    return s;
+}
+Console.WriteLine("Введите количество элементов массива: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+int[] arr = new int[size];
+
+FillArray(arr, 1, 9);
+PrintArray(arr);
+int x = Multy(arr);
+Console.WriteLine($"Произведение массива равно: {x}");
