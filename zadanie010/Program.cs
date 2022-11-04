975dcff6b272b996b1940d54fb51165bcd52c886
Aleksandr Onufriev <pskmp@yandex.ru>
2022-11-04 15:45:01 +0300

    dobavil zadanie010

---
+++ zadanie010/Program.cs
@@ -0,0 +1,33 @@
+﻿// I. Реализовать следующие функции для работы с массивами:
+// 10. Подсчёт количества вхождений элемента в массив
+Console.Clear();
+void FillArray(int[] arr, int min, int max)    
+    {
+        for (int i=0; i<arr.Length; i++)
+            arr[i] = new Random().Next(min, max);
+    }
+void PrintArray(int[] arr)                     
+{
+    for (int i=0; i<arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+int Count(int[] arr, int n)                   
+{
+    int count=0;
+    for (int i=0; i<arr.Length; i++)
+    {
+        if (arr[i]==n)
+        count++;
+    }
+    return count;
+}
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+int[] arr = new int [size];
+FillArray(arr, 0, 10);
+PrintArray(arr);
+Console.WriteLine("Введите искомое число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int x = Count(arr, n);
+Console.WriteLine($"Число {n} встречается в массиве {x} раз ");
