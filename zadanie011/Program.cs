aead39a7a961a0ded982a0e267b95e06c783f781
Aleksandr Onufriev <pskmp@yandex.ru>
2022-11-04 15:48:01 +0300

    dobavil zadanie011

---
+++ zadanie011/Program.cs
@@ -0,0 +1,33 @@
+﻿// I. Реализовать следующие функции для работы с массивами:
+// 11. Подсчёт количества чётных элементов в массив
+Console.Clear();
+void FillArray(int[] arr, int min, int max)  
+    {
+        for (int i=0; i<arr.Length; i++)
+            arr[i] = new Random().Next(min, max);
+    }
+
+
+void PrintArray(int[] arr)                     
+{
+    for (int i=0; i<arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+int EvenCount(int[] arr)                   
+{
+    int count=0;
+    for (int i=0; i<arr.Length; i++)
+    {
+        if (arr[i]%2==0)
+        count++;
+    }
+    return count;
+}
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+int[] arr = new int [size];
+FillArray(arr, 1, 20);
+PrintArray(arr);
+int x = EvenCount(arr);
+Console.WriteLine($"Количество четных элементов в массиве равно: {x} ");
